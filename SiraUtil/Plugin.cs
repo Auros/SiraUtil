10d26a61419c05e268a169bf4c4f62a31c0a8835
Auros Nexus <41306347+Auros@users.noreply.github.com>
2020-06-14 11:57:48 -0400

    Initial project setup

---
+++ SiraUtil/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using IPA;
+using IPALogger = IPA.Logging.Logger;
+
+namespace SiraUtil
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; set; }
+
+        [Init]
+        public Plugin(IPALogger logger)
+        {
+            Instance = this;
+            Log = logger;
+        }
+
+        [OnEnable]
+        public void OnEnable()
+        {
+
+        }
+
+        [OnDisable]
+        public void OnDisable()
+        {
+
+        }
+
+    }
+}
