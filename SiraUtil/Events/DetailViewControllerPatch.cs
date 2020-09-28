6cb1d6f6f2c3568814f20a482d58aa34783aa24f
Auros Nexus <aurosnex@gmail.com>
2020-09-28 01:48:30 -0400

    New SiraEvents

--- SiraUtil/Events/DetailViewControllerPatch.cs
+++
@@ -1,9 +0,0 @@
-﻿using HarmonyLib;
-
-namespace SiraUtil.Events
-{
-    internal class DetailViewControllerPatch
-    {
-        
-    }
-}
--- SiraUtil/Events/SiraEvents.cs
+++ SiraUtil/Events/SiraEvents.cs
@@ -1,9 +1,28 @@
 ﻿using System;
+using Zenject;
 
 namespace SiraUtil.Events
 {
-    internal static class SiraEvents
+    public class SiraEvents
     {
-        
+        internal static event EventHandler<SceneContextInstalledArgs> ContextInstalling;
+
+        internal static void SendInstallEvent(string name, SceneContext context, DiContainer container, bool post = true)
+        {
+            var args = new SceneContextInstalledArgs(name, container);
+            ContextInstalling.Invoke(context, args);
+        }
+
+        internal class SceneContextInstalledArgs : EventArgs
+        {
+            public string Name { get; }
+            public DiContainer Container { get; }
+
+            public SceneContextInstalledArgs(string name, DiContainer container)
+            {
+                Name = name;
+                Container = container;
+            }
+        }
     }
 }
