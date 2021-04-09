3e939597752de299baf1463368a6922cc5347554
Auros Nexus <aurosnex@gmail.com>
2021-04-09 10:51:32 -0400

    Added Test Project

---
+++ SiraUtil.Suite/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using IPA;
+using SiraUtil;
+using SiraUtil.Zenject;
+using IPALogger = IPA.Logging.Logger;
+
+namespace SiraUtil.Suite
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        [Init]
+        public Plugin(IPALogger logger)
+        {
+
+        }
+
+        [OnEnable]
+        public void OnEnable()
+        {
+
+        }
+
+        [OnDisable]
+        public void OnDisable()
+        {
+
+        }
+    }
+}
