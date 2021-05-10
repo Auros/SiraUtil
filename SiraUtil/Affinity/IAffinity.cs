9b3fd330311738e193091f63b757dcd994b16ba9
Auros Nexus <aurosnex@gmail.com>
2021-05-10 08:30:54 -0400

    Create IAffinity.cs

---
+++ SiraUtil/Affinity/IAffinity.cs
@@ -0,0 +1,10 @@
+﻿namespace SiraUtil.Affinity
+{
+    /// <summary>
+    /// A marking interface used to collect Affinity registrations in Zenject.
+    /// </summary>
+    public interface IAffinity
+    {
+
+    }
+}
