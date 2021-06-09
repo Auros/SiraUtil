5434cbd3908e6fb7c904d0c29cfd103024bd6743
Auros Nexus <aurosnex@gmail.com>
2021-06-09 09:48:11 -0400

    HTTP Skeleton

---
+++ SiraUtil/Web/IHttpResponse.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace SiraUtil.Web
+{
+    /// <summary>
+    /// An interface which describes the stat of an HTTP response.
+    /// </summary>
+    public interface IHttpResponse
+    {
+        /// <summary>
+        /// The HTTP status code of the response.
+        /// </summary>
+        int Code { get; }
+
+        /// <summary>
+        /// Whether or not the reuqest was successful or not.
+        /// </summary>
+        bool Successful { get; }
+
+        /// <summary>
+        /// Read the body as a stream.
+        /// </summary>
+        /// <returns>A stream of the response body.</returns>
+        Task<Stream> ReadAsStreamAsync();
+
+        /// <summary>
+        /// Read the body as a string.
+        /// </summary>
+        /// <returns>The body represented as a string.</returns>
+        Task<string> ReadAsStringAsync();
+
+        /// <summary>
+        /// Read the body as a byte array.
+        /// </summary>
+        /// <returns>The body represented as an array of bytes.</returns>
+        Task<byte[]> ReadAsByteArrayAsync();
+
+        /// <summary>
+        /// The error of the response. This will first try to grab the error from an 'error' json field at the root, then go to the http response, and then will go to the native client. Will be null if the request was successful.
+        /// </summary>
+        Task<string?> Error();
+    }
+}
---
+++ SiraUtil/Web/IHttpService.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SiraUtil.Web
+{
+    /// <summary>
+    /// An interface for making asynchronous HTTP calls to the internet.
+    /// </summary>
+    /// <remarks>
+    /// This is a warning. Although this class is public, it is not meant to be inherited normally. Please ONLY receive this through Zenject and do not create your own instance.
+    /// Future SiraUtil updates might add more to this interface.
+    /// </remarks>
+    public interface IHttpService
+    {
+        /// <summary>
+        /// The Authroization Bearer token for your requests (primarily used for auth). Defaults to null.
+        /// </summary>
+        string? Token { set; }
+
+        /// <summary>
+        /// The base URL to prefix all requests with. Defaults to null.
+        /// </summary>
+        string? BaseURL { get;  set; }
+
+        /// <summary>
+        /// The user agent for your requests. By default, it will be set to: '[Mod Name]/[Mod Version] ([IHttpService Provider Name]; [SiraUtil Version]; Beat Saber; [Beat Saber Version])  
+        /// </summary>
+        string UserAgent { get; set; }
+
+        /// <summary>
+        /// The headers for your requests. Token and UserAgent are synchronized with this.
+        /// </summary>
+        IDictionary<string, string> Headers { get; }
+
+        /// <summary>
+        /// Creates a HTTP GET request.
+        /// </summary>
+        /// <param name="url">The URL to send the request to.</param>
+        /// <param name="progress"></param>
+        /// <returns>The response.</returns>
+        Task<IHttpResponse> GetAsync(string url, IProgress<float>? progress = null);
+
+        /// <summary>
+        /// Creates a HTTP POST request.
+        /// </summary>
+        /// <param name="url">The URL to send the request to.</param>
+        /// <param name="body">The content to include as a UTF-8 JSON body. The object put in here will be automatically serialized.</param>
+        /// <returns>The response.</returns>
+        Task<IHttpResponse> PostAsync(string url, object? body = null);
+
+        /// <summary>
+        /// Creates a HTTP POST request.
+        /// </summary>
+        /// <param name="url">The URL to send the request to.</param>
+        /// <param name="rawBody">The content to include as a body. Anything put in here will be uploaded as is.</param>
+        /// <returns>The response.</returns>
+        Task<IHttpResponse> PostAsync(string url, string rawBody);
+
+        /// <summary>
+        /// Creates a HTTP POST request.
+        /// </summary>
+        /// <param name="url">The URL to send the request to.</param>
+        /// <param name="content">The body (as a stream) for file uploads.</param>
+        /// <param name="contentName">The name of the content, if applicable.</param>
+        /// <returns>The response.</returns>
+        Task<IHttpResponse> PostAsync(string url, Stream content, string? contentName = null);
+
+        /// <summary>
+        /// Creates a HTTP PUT request.
+        /// </summary>
+        /// <param name="url">The URL to send the request to.</param>
+        /// <param name="body">The content to include as a UTF-8 JSON body. The object put in here will be automatically serialized.</param>
+        /// <returns>The response.</returns>
+        Task<IHttpResponse> PutAsync(string url, object? body = null);
+
+        /// <summary>
+        /// Creates a HTTP PUT request.
+        /// </summary>
+        /// <param name="url">The URL to send the request to.</param>
+        /// <param name="rawBody">The content to include as a body. Anything put in here will be uploaded as is.</param>
+        /// <returns>The response.</returns>
+        Task<IHttpResponse> PutAsync(string url, string rawBody);
+
+        /// <summary>
+        /// Creates a HTTP PATCH request.
+        /// </summary>
+        /// <param name="url">The URL to send the request to.</param>
+        /// <param name="body">The content to include as a UTF-8 JSON body. The object put in here will be automatically serialized.</param>
+        /// <returns>The response.</returns>
+        Task<IHttpResponse> PatchAsync(string url, object? body = null);
+
+        /// <summary>
+        /// Creates a HTTP PATCH request.
+        /// </summary>
+        /// <param name="url">The URL to send the request to.</param>
+        /// <param name="rawBody">The content to include as a body. Anything put in here will be uploaded as is.</param>
+        /// <returns>The response.</returns>
+        Task<IHttpResponse> PatchAsync(string url, string rawBody);
+
+        /// <summary>
+        /// Creates a HTTP DELETE request.
+        /// </summary>
+        /// <param name="url">The URL to send the request to.</param>
+        /// <returns>The response.</returns>
+        Task<IHttpResponse> DeleteAsync(string url);
+    }
+}
