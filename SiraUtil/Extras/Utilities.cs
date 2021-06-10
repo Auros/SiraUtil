c8164836993dc65b4647fdc6ef2e7739e60d19ce
Auros Nexus <aurosnex@gmail.com>
2021-06-10 01:53:04 -0400

    Localization Stuff

---
+++ SiraUtil/Extras/Utilities.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace SiraUtil.Extras
+{
+    /// <summary>
+    /// A collection of utilities used in SiraUtil.
+    /// </summary>
+    public static class Utilities
+    {
+        /// <summary>
+        /// Check if the following code instructions starting from a given index match a list of opcodes.
+        /// </summary>
+        /// <param name="codes">A list of code instructions to check</param>
+        /// <param name="toCheck">A list of op codes that is expected to match</param>
+        /// <param name="startIndex">Index to start checking from (inclusive)</param>
+        /// <returns>Whether or not the op codes found in the code instructions match.</returns>
+        public static bool OpCodeSequence(List<CodeInstruction> codes, List<OpCode> toCheck, int startIndex)
+        {
+            for (int i = 0; i < toCheck.Count; i++)
+            {
+                if (codes[startIndex + i].opcode != toCheck[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ SiraUtil/Tweaks/RemoveLocalizationLog.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using Polyglot;
+using SiraUtil.Extras;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+
+namespace SiraUtil.Tweaks
+{
+    [HarmonyPatch(typeof(LocalizationImporter), "ImportTextFile")]
+    internal static class RemoveLocalizationLog
+    {
+        private static readonly List<OpCode> _logOpCodes = new()
+        {
+             OpCodes.Ldstr,
+             OpCodes.Ldloc_S,
+             OpCodes.Ldstr,
+             OpCodes.Call,
+             OpCodes.Call
+        };
+
+        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = instructions.ToList();
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (Utilities.OpCodeSequence(codes, _logOpCodes, i))
+                {
+                    codes.RemoveRange(i, _logOpCodes.Count);
+                    break;
+                }
+            }
+            return codes.AsEnumerable();
+        }
+    }
+}
