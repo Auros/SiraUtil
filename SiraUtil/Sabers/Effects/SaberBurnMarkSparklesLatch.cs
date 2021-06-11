ffd0d15c8ee10ee5e8d9da7ff6ed8b25ee7c2d1b
Auros Nexus <aurosnex@gmail.com>
2021-06-10 21:31:31 -0400

    Sparkles Patch: 2 -> Length

---
+++ SiraUtil/Sabers/Effects/SaberBurnMarkSparklesLatch.cs
@@ -0,0 +1,53 @@
+﻿using HarmonyLib;
+using IPA.Utilities;
+using SiraUtil.Affinity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Zenject;
+
+namespace SiraUtil.Sabers.Effects
+{
+    internal class SaberBurnMarkSparklesLatch : IInitializable, IDisposable, IAffinity
+    {
+        private static FieldAccessor<SaberBurnMarkSparkles, Saber[]>.Accessor Sabers = FieldAccessor<SaberBurnMarkSparkles, Saber[]>.GetAccessor("_sabers");
+        private static FieldAccessor<SaberBurnMarkSparkles, Vector3[]>.Accessor PreviousMarks = FieldAccessor<SaberBurnMarkSparkles, Vector3[]>.GetAccessor("_prevBurnMarkPos");
+        private static FieldAccessor<SaberBurnMarkSparkles, bool[]>.Accessor PreviousMarksValid = FieldAccessor<SaberBurnMarkSparkles, bool[]>.GetAccessor("_prevBurnMarkPosValid");
+        private static FieldAccessor<SaberBurnMarkSparkles, ParticleSystem[]>.Accessor Particles = FieldAccessor<SaberBurnMarkSparkles, ParticleSystem[]>.GetAccessor("_burnMarksPS");
+        private static FieldAccessor<SaberBurnMarkSparkles, ParticleSystem.EmissionModule[]>.Accessor Emissions = FieldAccessor<SaberBurnMarkSparkles, ParticleSystem.EmissionModule[]>.GetAccessor("_burnMarksEmissionModules");
+
+        private readonly SiraSaberFactory _siraSaberFactory;
+        private readonly SaberModelManager _saberModelManager;
+
+        public SaberBurnMarkSparklesLatch(SiraSaberFactory siraSaberFactory, SaberModelManager saberModelManager)
+        {
+            _siraSaberFactory = siraSaberFactory;
+            _saberModelManager = saberModelManager;
+        }
+
+        public void Initialize()
+        {
+            _siraSaberFactory.SaberCreated += SiraSaberFactory_SaberCreated;
+        }
+
+        private void SiraSaberFactory_SaberCreated(SiraSaber siraSaber)
+        {
+
+        }
+
+        public void Dispose()
+        {
+            _siraSaberFactory.SaberCreated -= SiraSaberFactory_SaberCreated;
+        }
+
+        [AffinityTranspiler]
+        [AffinityPatch(typeof(SaberBurnMarkSparkles), nameof(SaberBurnMarkSparkles.LateUpdate))]
+        internal IEnumerable<CodeInstruction> ShiftColorReceiver(IEnumerable<CodeInstruction> instructions)
+        {
+
+        }
+    }
+}
---
+++ SiraUtil/Sabers/Effects/SaberBurnMarkSparklesPatch.cs
@@ -0,0 +1,67 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+
+namespace SiraUtil.Sabers.Effects
+{
+    [HarmonyPatch(typeof(SaberBurnMarkSparkles))]
+    internal class SaberBurnMarkSparklesPatch
+    {
+        private static readonly MethodInfo _setStartColor = typeof(ParticleSystem.EmitParams).GetMethod("set_startColor", BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic);
+
+        [HarmonyTranspiler]
+        [HarmonyPatch(nameof(SaberBurnMarkSparkles.OnEnable))]
+        internal static IEnumerable<CodeInstruction> DynamicEnable(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> codes = instructions.ToList();
+            ProcessEnableDisable(ref codes);
+            return codes;
+        }
+
+        [HarmonyTranspiler]
+        [HarmonyPatch(nameof(SaberBurnMarkSparkles.OnDisable))]
+        internal static IEnumerable<CodeInstruction> DynamicDisable(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> codes = instructions.ToList();
+            ProcessEnableDisable(ref codes);
+            return codes;
+        }
+
+        [HarmonyTranspiler]
+        [HarmonyPatch(nameof(SaberBurnMarkSparkles.LateUpdate))]
+        internal static IEnumerable<CodeInstruction> DynamicLateUpdate(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> codes = instructions.ToList();
+            ProcessEnableDisable(ref codes);
+            return codes;
+        }
+
+        private static void ProcessEnableDisable(ref List<CodeInstruction> codes)
+        {
+            object? burnMarks = null;
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Ldfld && burnMarks is null) // We collect the operand that we are going to be using (_burnMarksPS or _sabers)
+                    burnMarks = codes[i].operand;
+
+                if (codes[i].opcode == OpCodes.Ldc_I4_2)
+                {
+                    codes.RemoveAt(i); // Remove the '2' being pushed onto the stack
+                    codes.InsertRange(i, new CodeInstruction[] // And use the Length property of the array
+                    {
+                        new CodeInstruction(OpCodes.Ldarg_0),
+                        new CodeInstruction(OpCodes.Ldfld, burnMarks), // this needs operand of _burnMarksPs or _sabers
+                        new CodeInstruction(OpCodes.Ldlen),
+                        new CodeInstruction(OpCodes.Conv_I4)
+                    });
+                    break;
+                }
+            }
+
+        }
+    }
+}
