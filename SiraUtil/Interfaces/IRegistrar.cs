b06f6d1eed47b30678f5ff3546d4eeebf5b77b5c
Auros Nexus <aurosnex@gmail.com>
2021-06-12 13:19:20 -0400

    this interface is ok actually

---
+++ SiraUtil/Interfaces/IRegistrar.cs
@@ -0,0 +1,21 @@
+﻿namespace SiraUtil.Interfaces
+{
+    /// <summary>
+    /// A generic interface which defines a registration.
+    /// </summary>
+    /// <typeparam name="T">The type to register.</typeparam>
+    public interface IRegistrar<T>
+    {
+        /// <summary>
+        /// Adds a registration to this registrar.
+        /// </summary>
+        /// <param name="value"></param>
+        void Add(T value);
+
+        /// <summary>
+        /// Removes a registration from this registrar.
+        /// </summary>
+        /// <param name="value"></param>
+        void Remove(T value);
+    }
+}
