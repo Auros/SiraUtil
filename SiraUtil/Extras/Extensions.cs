56979a2c0ca8dbee693037c3f9b919a0d57ebb48
Auros Nexus <aurosnex@gmail.com>
2021-06-12 14:05:53 -0400

    FromNewComponentAsViewController

--- SiraUtil/Extras/Extensions.cs
+++ SiraUtil/Extras/Extensions.cs
@@ -1,5 +1,13 @@
-﻿using SiraUtil.Objects;
+﻿using HMUI;
+using IPA.Utilities;
+using SiraUtil.Objects;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using VRUIControls;
 using Zenject;
+using Object = UnityEngine.Object;
 
 namespace SiraUtil.Extras
 {
@@ -20,6 +28,56 @@ namespace SiraUtil.Extras
         public static void RegisterRedecorator<TRegistrator>(this DiContainer container, TRegistrator registrator) where TRegistrator : RedecoratorRegistration
         {
             container.AncestorContainers[0].Bind<RedecoratorRegistration>().FromInstance(registrator).AsCached();
+        }
+
+        /// <summary>
+        /// Binds a view controller to the container.
+        /// </summary>
+        /// <param name="binder">The binder.</param>
+        /// <param name="onInstantiated">The callback when the view controller is instantiated.</param>
+        /// <returns></returns>
+        public static ScopeConcreteIdArgConditionCopyNonLazyBinder FromNewComponentAsViewController(this FromBinder binder, Action<InjectContext, object> onInstantiated = null!)
+        {
+            var go = new GameObject("ViewController");
+
+            go.gameObject.SetActive(false);
+            var canvas = go.AddComponent<Canvas>();
+            canvas.additionalShaderChannels |= AdditionalCanvasShaderChannels.Normal;
+            canvas.additionalShaderChannels |= AdditionalCanvasShaderChannels.TexCoord1;
+            canvas.additionalShaderChannels |= AdditionalCanvasShaderChannels.TexCoord2;
+            canvas.additionalShaderChannels |= AdditionalCanvasShaderChannels.Tangent;
+
+            var raycaster = go.AddComponent<DummyRaycaster>();
+            var componentBinding = binder.FromNewComponentOn(go);
+            raycaster.enabled = false;
+
+            componentBinding.OnInstantiated((ctx, obj) =>
+            {
+                if (obj is ViewController vc)
+                {
+                    var newRaycaster = go.AddComponent<VRGraphicRaycaster>();
+                    Object.Destroy(raycaster);
+                    var cache = ctx.Container.Resolve<PhysicsRaycasterWithCache>();
+                    newRaycaster.SetField("_physicsRaycaster", cache);
+                    go.name = vc.GetType().Name;
+                    var rt = vc.rectTransform;
+                    rt.localEulerAngles = Vector3.zero;
+                    rt.localScale = rt.anchorMax = Vector3.one;
+                    rt.anchorMin = rt.sizeDelta = Vector2.zero;
+                }
+                onInstantiated?.Invoke(ctx, obj);
+            });
+            return componentBinding;
+        }
+
+        internal class DummyRaycaster : BaseRaycaster
+        {
+            public override Camera eventCamera => Camera.main;
+
+            public override void Raycast(PointerEventData eventData, List<RaycastResult> resultAppendList)
+            {
+
+            }
         }
     }
 }
