1a8d9c22a93fb04b8f2bf4bc56a8ef7a116398ed
Auros Nexus <aurosnex@gmail.com>
2021-06-12 13:17:35 -0400

    Re-added the best of the old interfaces
    " hmmmm today i will add stateless toggleability to sirautil
    ^ clueless " - Caeden
    
    Co-Authored-By: Caeden117 <caeden.s@outlook.com>

---
+++ SiraUtil/Interfaces/IDismissable.cs
@@ -0,0 +1,13 @@
+﻿namespace SiraUtil.Interfaces
+{
+    /// <summary>
+    /// Interface for dismissing objects.
+    /// </summary>
+    public interface IDismissable
+    {
+        /// <summary>
+        /// Dismisses an object.
+        /// </summary>
+        void Dismiss();
+    }
+}
