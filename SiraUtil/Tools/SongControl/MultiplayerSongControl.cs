2d459af194db5104b51083d7d01931cba51ed262
Auros Nexus <aurosnex@gmail.com>
2021-10-23 20:11:22 -0400

    for handling spectators, we optionally inject the view controller since there's no guarantee it'll be in use

--- SiraUtil/Tools/SongControl/MultiplayerSongControl.cs
+++ SiraUtil/Tools/SongControl/MultiplayerSongControl.cs
@@ -1,34 +1,36 @@
-﻿namespace SiraUtil.Tools.SongControl
+﻿using Zenject;
+
+namespace SiraUtil.Tools.SongControl
 {
     internal class MultiplayerSongControl : ISongControl
     {
-        //private readonly MultiplayerLocalActivePlayerInGameMenuViewController _multiplayerLocalActivePlayerInGameMenuViewController;
-        //
-        //internal MultiplayerSongControl(MultiplayerLocalActivePlayerInGameMenuViewController multiplayerLocalActivePlayerInGameMenuViewController)
-        //{
-        //    _multiplayerLocalActivePlayerInGameMenuViewController = multiplayerLocalActivePlayerInGameMenuViewController;
-        //}
+        private readonly MultiplayerLocalActivePlayerInGameMenuViewController? _multiplayerLocalActivePlayerInGameMenuViewController;
+        
+        internal MultiplayerSongControl([InjectOptional] MultiplayerLocalActivePlayerInGameMenuViewController multiplayerLocalActivePlayerInGameMenuViewController)
+        {
+            _multiplayerLocalActivePlayerInGameMenuViewController = multiplayerLocalActivePlayerInGameMenuViewController;
+        }
 
-        public bool IsPaused => false;// _multiplayerLocalActivePlayerInGameMenuViewController.isActiveAndEnabled;
+        public bool IsPaused => _multiplayerLocalActivePlayerInGameMenuViewController != null ? _multiplayerLocalActivePlayerInGameMenuViewController.isActiveAndEnabled : false;
 
         public void Continue()
         {
-            //_multiplayerLocalActivePlayerInGameMenuViewController.ResumeButtonPressed();
+            _multiplayerLocalActivePlayerInGameMenuViewController?.ResumeButtonPressed();
         }
 
         public void Pause()
         {
-           // _multiplayerLocalActivePlayerInGameMenuViewController.ShowMenu();
+            _multiplayerLocalActivePlayerInGameMenuViewController?.ShowMenu();
         }
 
         public void Quit()
         {
-            //_multiplayerLocalActivePlayerInGameMenuViewController.GiveUpButtonPressed();
+            _multiplayerLocalActivePlayerInGameMenuViewController?.GiveUpButtonPressed();
         }
 
         public void Restart()
         {
-            //_multiplayerLocalActivePlayerInGameMenuViewController.GiveUpButtonPressed();
+            _multiplayerLocalActivePlayerInGameMenuViewController?.GiveUpButtonPressed();
         }
     }
 }
