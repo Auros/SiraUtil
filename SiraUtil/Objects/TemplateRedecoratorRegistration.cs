74a15622a640153533c03721e568f1a90f17c8b6
Auros Nexus <41306347+Auros@users.noreply.github.com>
2021-12-10 09:07:19 -0500

    Exposed constructor of the object redecorator system.

--- SiraUtil/Objects/TemplateRedecoratorRegistration.cs
+++ SiraUtil/Objects/TemplateRedecoratorRegistration.cs
@@ -9,15 +9,28 @@ namespace SiraUtil.Objects
     /// <typeparam name="TParentType">The type of the parent.</typeparam>
     public abstract class TemplateRedecoratorRegistration<TPrefabType, TParentType> : RedecoratorRegistration
     {
-
         private readonly Func<TPrefabType, TPrefabType> _redecorateCall;
 
-        internal TemplateRedecoratorRegistration(string contract, Func<TPrefabType, TPrefabType> redecorateCall, int priority = 0, bool chain = true) : base(contract, typeof(TPrefabType), typeof(TParentType), priority, chain)
+        /// <summary>
+        /// Creates a new templated redecorator.
+        /// </summary>
+        /// <param name="contract">The contract type of the redecorator.</param>
+        /// <param name="redecorateCall">The callback for redecoration.</param>
+        /// <param name="priority">The redecoration priority.</param>
+        /// <param name="chain">Whether to chain this redecoration with others. Every redecoration is now aggregated.
+        /// The chain will start if the highest priority object has chaining enabled and will stop once a registration
+        /// in the aggregate has chaining disabled.</param>
+        public TemplateRedecoratorRegistration(string contract, Func<TPrefabType, TPrefabType> redecorateCall, int priority = 0, bool chain = true) : base(contract, typeof(TPrefabType), typeof(TParentType), priority, chain)
         {
             _redecorateCall = redecorateCall;
         }
 
-        internal override object Redecorate(object value)
+        /// <summary>
+        /// Redecorates through a template.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        protected internal override object Redecorate(object value)
         {
             return _redecorateCall.Invoke(((TPrefabType)value)!)!;
         }
