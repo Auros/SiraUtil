199896ad358c62b793366ef4ab900a6868f22eb2
Nicolas Gnyra <nicolas@gnyra.com>
2024-11-15 22:41:12 -0500

    Fix SteamVR not behaving properly when recentering is allowed

---
+++ SiraUtil/Tweaks/DisableOpenXRRecentering.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using UnityEngine.XR.OpenXR;
+
+namespace SiraUtil.Tweaks
+{
+    /// <summary>
+    /// This patch fixes an issue with SteamVR where Unity will decide to not use SteamVR's room center
+    /// and instead try to invent its own origin, often resulting in the headset being under the floor.
+    /// This tends to happen when using FPFCToggle.
+    /// </summary>
+    /// <remarks>
+    /// This post explains the various possible OpenXR tracking spaces: <see href="https://discussions.unity.com/t/how-to-recenter-in-openxr/935209/9"/>
+    /// </remarks>
+    [HarmonyPatch(typeof(OpenXRLoaderBase), nameof(OpenXRLoaderBase.Initialize))]
+    internal static class DisableOpenXRRecentering
+    {
+        private static void Postfix(ref bool __result)
+        {
+            if (__result && OpenXRRuntime.name == "SteamVR/OpenXR")
+            {
+                Plugin.Log.Info("Disabling recentering");
+                OpenXRSettings.SetAllowRecentering(false, 0);
+            }
+        }
+    }
+}
