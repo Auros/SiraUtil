b4cc65a5aa34d8793185df02cf9a06b8db3e3b58
Nicolas Gnyra <nicolas@gnyra.com>
2024-12-11 21:15:15 -0500

    Show full type name and assembly name in Zenject exceptions

---
+++ SiraUtil/Tweaks/ResolveExceptionDetailer.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using ModestTree;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace SiraUtil.Tweaks
+{
+    [HarmonyPatch(typeof(TypeStringFormatter), nameof(TypeStringFormatter.PrettyNameInternal))]
+    internal static class ResolveExceptionDetailer
+    {
+        private static readonly MethodInfo MemberInfoNameGetter = AccessTools.DeclaredPropertyGetter(typeof(MemberInfo), nameof(MemberInfo.Name));
+        private static readonly MethodInfo FullNameAndAssemblyMethod = AccessTools.DeclaredMethod(typeof(ResolveExceptionDetailer), nameof(FullNameAndAssembly));
+
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return new CodeMatcher(instructions)
+                .MatchForward(false, new CodeMatch(i => i.Calls(MemberInfoNameGetter)))
+                .SetAndAdvance(OpCodes.Call, FullNameAndAssemblyMethod)
+                .InstructionEnumeration();
+        }
+
+        private static string FullNameAndAssembly(Type type) => $"{type.FullName}, {type.Assembly.GetName().Name}";
+    }
+}
