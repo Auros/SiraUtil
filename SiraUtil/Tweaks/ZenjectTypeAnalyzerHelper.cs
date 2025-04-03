0789b8831ef872f91c3fbd6875ccb8d06cce0004
Nicolas Gnyra <nicolas@gnyra.com>
2025-04-03 17:19:58 -0400

    Rethrow Zenject reflection exceptions with more details

---
+++ SiraUtil/Tweaks/ZenjectTypeAnalyzerHelper.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using System;
+using Zenject;
+
+namespace SiraUtil.Tweaks
+{
+    [HarmonyPatch]
+    internal class ZenjectTypeAnalyzerHelper
+    {
+        [HarmonyPatch(typeof(TypeAnalyzer), nameof(TypeAnalyzer.TryGetInfo), new[] { typeof(Type) })]
+        [HarmonyFinalizer]
+        private static Exception? TypeAnalyzer_TryGetInfo(Type type, Exception __exception)
+        {
+            if (__exception != null)
+            {
+                return new TypeAnalyzerException($"Failed to get type info for type {type.FullDescription()}", __exception);
+            }
+
+            return __exception;
+        }
+    }
+
+    /// <summary>
+    /// Exception thrown when an error occurs within <see cref="TypeAnalyzer"/>.
+    /// </summary>
+    public class TypeAnalyzerException : Exception
+    {
+        internal TypeAnalyzerException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
