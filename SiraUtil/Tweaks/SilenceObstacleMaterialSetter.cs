33548ddc7529ab95afdef007d3ece6867287dd96
Nicolas Gnyra <nicolas@gnyra.com>
2025-04-14 17:07:35 -0400

    Stop ObstacleMaterialSetter log spam

---
+++ SiraUtil/Tweaks/SilenceObstacleMaterialSetter.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+
+namespace SiraUtil.Tweaks
+{
+    [HarmonyPatch(typeof(ObstacleMaterialSetter), nameof(ObstacleMaterialSetter.SetCoreMaterial))]
+    internal class SilenceObstacleMaterialSetter
+    {
+        private static readonly MethodInfo DebugLogWarningMethod = AccessTools.DeclaredMethod(typeof(Debug), nameof(Debug.LogWarning), new System.Type[] { typeof(object) });
+
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return new CodeMatcher(instructions)
+                .MatchForward(
+                    false,
+                    new CodeMatch(OpCodes.Ldstr),
+                    new CodeMatch(i => i.Calls(DebugLogWarningMethod)))
+                .SetAndAdvance(OpCodes.Nop, null) // keep the label
+                .RemoveInstruction()
+                .InstructionEnumeration();
+        }
+    }
+}
