fbcd68dc4b4de2331babeafaee4ecf1b13d12bcc
Nicolas Gnyra <nicolas@gnyra.com>
2025-12-18 23:06:58 -0500

    Catch exceptions when applying Affinity patches

--- SiraUtil/Affinity/AffinityKernel.cs
+++ SiraUtil/Affinity/AffinityKernel.cs
@@ -1,4 +1,7 @@
-﻿using SiraUtil.Affinity.Harmony;
+﻿using HarmonyLib;
+using SiraUtil.Affinity.Harmony;
+using SiraUtil.Logging;
+using System;
 using Zenject;
 
 namespace SiraUtil.Affinity
@@ -8,12 +11,21 @@ namespace SiraUtil.Affinity
         private readonly AffinityManager _affinityManager;
         private readonly IAffinityPatcher _affinityPatcher = new HarmonyAffinityPatcher();
 
-        public AffinityKernel([InjectLocal] AffinityManager affinityManager)
+        public AffinityKernel([InjectLocal] AffinityManager affinityManager, SiraLog logger)
         {
             _affinityManager = affinityManager;
 
             foreach (var affinity in _affinityManager.Affinities)
-                _affinityPatcher.Patch(affinity);
+            {
+                try
+                {
+                    _affinityPatcher.Patch(affinity);
+                }
+                catch (Exception ex)
+                {
+                    logger.Error($"Failed to apply Affinity patches on class '{affinity.GetType().FullDescription()}'\n{ex}");
+                }
+            }
         }
 
         public void LateDispose()
