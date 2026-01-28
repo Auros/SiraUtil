2129bc442014e93d5a917a1d0b8dd4f7a990cc75
Nicolas Gnyra <nicolas@gnyra.com>
2026-01-27 22:34:59 -0500

    Rework how FPFC is toggled

---
+++ SiraUtil/Tweaks/FixTrackedPoseDriverEnableDisablePosition.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using SiraUtil.Tools.FPFC;
+using UnityEngine.SpatialTracking;
+
+namespace SiraUtil.Tweaks
+{
+    [HarmonyPatch(typeof(TrackedPoseDriver))]
+    internal class FixTrackedPoseDriverEnableDisablePosition
+    {
+        [HarmonyPatch(nameof(TrackedPoseDriver.OnEnable))]
+        [HarmonyPostfix]
+        private static void Postfix(TrackedPoseDriver __instance)
+        {
+            // conservatively apply this patch only to the main camera
+            if (__instance.TryGetComponent(out MainCamera _))
+            {
+                __instance.PerformUpdate();
+            }
+        }
+
+        // conservatively only apply this patch if the camera controller has been disabled before the OnDisable() that calls this method
+        [HarmonyPatch(nameof(TrackedPoseDriver.ResetToCachedLocalPosition))]
+        [HarmonyPrefix]
+        private static bool Prefix(TrackedPoseDriver __instance) => !__instance.TryGetComponent(out CameraController cameraController) || !cameraController.enabled;
+    }
+}
