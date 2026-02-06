eca6d73f25d9f89a1b702c8f3ba5c9636227d8c2
Nicolas Gnyra <nicolas@gnyra.com>
2026-02-06 00:41:23 -0500

    Add `Active` property

--- SiraUtil/Tools/FPFC/FPFCSettingsController.cs
+++ SiraUtil/Tools/FPFC/FPFCSettingsController.cs
@@ -43,8 +43,11 @@ namespace SiraUtil.Tools.FPFC
 
                 Changed?.Invoke(this);
                 NotifyPropertyChanged();
+                NotifyPropertyChanged(nameof(Active));
             }
         }
+
+        public bool Active => Enabled && _cameraControllers.Count > 0;
 
         public bool LockViewOnDisable => _fpfcOptions.LockViewOnDisable;
 
@@ -99,6 +102,7 @@ namespace SiraUtil.Tools.FPFC
             ApplyState();
 
             Changed?.Invoke(this);
+            NotifyPropertyChanged(nameof(Active));
         }
 
         public void Remove(CameraController cameraController)
@@ -116,14 +120,13 @@ namespace SiraUtil.Tools.FPFC
             ApplyState();
 
             Changed?.Invoke(this);
+            NotifyPropertyChanged(nameof(Active));
         }
 
         private void ApplyState()
         {
-            bool active = Enabled && _cameraControllers.Count > 0;
-
-            Cursor.lockState = active ? CursorLockMode.Locked : CursorLockMode.None;
-            Cursor.visible = !active;
+            Cursor.lockState = Active ? CursorLockMode.Locked : CursorLockMode.None;
+            Cursor.visible = !Active;
 
             Application.targetFrameRate = Enabled && LimitFrameRate ? (int)Math.Round(Screen.currentResolution.refreshRateRatio.value) : -1;
             QualitySettings.vSyncCount = Enabled ? VSyncCount : 0;
