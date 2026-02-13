12c25ceaf8c08bf82ce34369793f6f64a025e49d
Nicolas Gnyra <nicolas@gnyra.com>
2026-02-12 20:46:42 -0500

    Force `TrackedPoseDriver` to update when enabled
    This prevents having the camera at (0, 0, 0) for a single frame when toggling FPFC

---
+++ SiraUtil/Tweaks/ForceTrackedPoseDriverUpdateOnEnable.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using UnityEngine.SpatialTracking;
+
+namespace SiraUtil.Tweaks
+{
+    [HarmonyPatch(typeof(TrackedPoseDriver), nameof(TrackedPoseDriver.OnEnable))]
+    internal class ForceTrackedPoseDriverUpdateOnEnable
+    {
+        private static void Postfix(TrackedPoseDriver __instance)
+        {
+            __instance.PerformUpdate();
+        }
+    }
+}
