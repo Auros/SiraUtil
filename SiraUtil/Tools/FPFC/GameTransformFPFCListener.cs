63f19025221a25bd9f16ec65fe540f3c9a67ee26
Nicolas Gnyra <nicolas@gnyra.com>
2026-02-13 21:40:28 -0500

    Ignore additional warning

--- SiraUtil/Tools/FPFC/GameTransformFPFCListener.cs
+++ SiraUtil/Tools/FPFC/GameTransformFPFCListener.cs
@@ -5,9 +5,9 @@ namespace SiraUtil.Tools.FPFC
     // this class needs to stay around until this patch in Heck is removed: https://github.com/Aeroluna/Heck/blob/7a0a9cfcac617b1dc14d3d646d3fc1acf2248bf2/Heck/HarmonyPatches/SiraUtilHeadFinder.cs
     internal class GameTransformFPFCListener : IFPFCListener
     {
-#pragma warning disable CS0414
+#pragma warning disable CS0414, IDE0051
         private readonly Transform? _originalHeadTransform = null;
-#pragma warning restore CS0414
+#pragma warning restore CS0414, IDE0051
 
         public void Enabled()
         {
