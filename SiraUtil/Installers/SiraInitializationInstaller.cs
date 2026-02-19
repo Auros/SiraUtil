faf44b7b1be8c3f50e2a1b165de32ad58eea3294
Nicolas Gnyra <nicolas@gnyra.com>
2026-02-19 08:01:35 -0500

    Don't iterate over ActiveZenjectors multiple times

--- SiraUtil/Installers/SiraInitializationInstaller.cs
+++ SiraUtil/Installers/SiraInitializationInstaller.cs
@@ -34,50 +34,37 @@ namespace SiraUtil.Installers
             Container.BindInterfacesTo<FPFCFixDaemon>().AsSingle();
             Container.BindInterfacesTo<InputSpoofFPFCListener>().AsSingle();
 
-            // Install all UBinders
+            Container.Bind<SiraLog>().AsTransient().OnInstantiated<SiraLog>(SiraLogCreated);
+            Container.Bind<Logger>().FromMethod(CreateIPAChildLogger).AsTransient();
+
+            _siraLogManager.Clear();
+            _httpServiceManager.Clear();
+            _siraSyncManager.Clear();
+
             foreach (Zenjector zenjector in _zenjectManager.ActiveZenjectors)
             {
                 if (zenjector.UBinderType is not null && zenjector.UBinderValue is not null)
                 {
                     Container.Bind(zenjector.UBinderType).FromInstance(zenjector.UBinderValue).AsSingle();
                 }
-            }
 
-            // Takes every active zenjector and adds them to the SiraLogger
-            _siraLogManager.Clear();
-            foreach (Zenjector zenjector in _zenjectManager.ActiveZenjectors)
-            {
                 if (zenjector.Logger is not null)
                 {
                     _siraLogManager.AddLogger(zenjector.Metadata.Assembly, zenjector.Logger);
                 }
-            }
 
-            Container.Bind<SiraLog>().AsTransient().OnInstantiated<SiraLog>(SiraLogCreated);
-            Container.Bind<Logger>().FromMethod(CreateIPAChildLogger).AsTransient();
-
-            // Takes every active zenjector and adds them to the http service.
-            _httpServiceManager.Clear();
-            foreach (Zenjector zenjector in _zenjectManager.ActiveZenjectors)
-            {
                 if (zenjector.HttpServiceType is not null)
                 {
                     _httpServiceManager.AddService(zenjector.Metadata.Assembly);
                 }
-            }
 
-            Container.Bind<IHttpService>().To<ContainerizedHttpService>().AsTransient().OnInstantiated<IHttpService>(HttpServiceCreated);
-
-            // Takes every active zenjector and adds them to the SiraSync service.
-            _siraSyncManager.Clear();
-            foreach (Zenjector zenjector in _zenjectManager.ActiveZenjectors)
-            {
                 if (zenjector.SiraSyncServiceType is not null)
                 {
                     _siraSyncManager.AddService(zenjector.Metadata.Assembly);
                 }
             }
 
+            Container.Bind<IHttpService>().To<ContainerizedHttpService>().AsTransient().OnInstantiated<IHttpService>(HttpServiceCreated);
             Container.Bind<ISiraSyncService>().To<ContainerizedSiraSyncService>().AsTransient().OnInstantiated<ISiraSyncService>(SiraSyncServiceCreated);
 
             // Bind any global services
